ca675e90bbd7574a92ec894ea2471011628396c2
tomm-rodion <autovolna@gmail.com>
2023-12-09 01:46:41 +0200

    layout app

---
+++ src/components/App/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+import Layout from 'components/Layout/Layout';
+
+const Home = lazy(() => {
+  import('../../pages/Home/Home');
+});
+const Catalog = lazy(() => {
+  import('../../pages/Catalog/Catalog');
+});
+const Favorites = lazy(() => {
+  import('../../pages/Favorites/Favorites');
+});
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
