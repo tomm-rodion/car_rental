db510859839b46f74b454d0d44686358122a4e95
tomm-rodion <autovolna@gmail.com>
2023-12-09 18:40:10 +0200

    add persistor and favicon

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: persistReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
