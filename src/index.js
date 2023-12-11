1b89be23be47ffe287c71dee0d324bda9d51a6cb
tomm-rodion <autovolna@gmail.com>
2023-12-11 22:49:20 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/rental-car">
+      <BrowserRouter basename="/car_rental">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
