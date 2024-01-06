0bec1b4f35f760f49b8e526166225772d81d9acd
tomm-rodion <autovolna@gmail.com>
2024-01-06 23:59:01 +0200

    Add type , test

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,6 +3,7 @@ import { StylesButton, Text } from './Button.styled';
 export default function Button({ text, onClick, width }) {
   return (
     <StylesButton
+      type="button"
       variant="contained"
       onClick={onClick}
       style={{ width: width }}
