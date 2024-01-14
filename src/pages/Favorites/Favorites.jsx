2038278a68f63598c4119c8f101295f1f01f2ad9
tomm-rodion <autovolna@gmail.com>
2024-01-14 23:54:07 +0200

    refactoring favorites page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -6,11 +6,17 @@ import { selectFavorites } from '../../redux/selectors';
 import { LoadMore } from 'pages/Catalog/Catalog.styled';
 import NavBar from 'components/NavBar/NavBar';
 
-export default function Favorites() {
+export const Favorites = () => {
+  // Використання селектора для отримання обраних автомобілів зі стану Redux
   const favoriteCars = useSelector(selectFavorites);
+
+  // Локальний стан для відстеження сторінки та списку автомобілів для відображення
   const [page, setPage] = useState(1);
   const [cars, setCars] = useState([]);
 
+  /**
+   * Функція для завантаження додаткових автомобілів при натисканні на кнопку "Завантажити ще".
+   */
   const loadMore = () => {
     const startIndex = page * 8;
     const nextCars = favoriteCars.favorites.slice(startIndex, startIndex + 8);
@@ -18,6 +24,10 @@ export default function Favorites() {
     setPage(page + 1);
   };
 
+  /**
+   * Ефект, який викликається при зміні обраних автомобілів.
+   * Ініціалізує список автомобілів для відображення на першій сторінці.
+   */
   useEffect(() => {
     const initialCars = favoriteCars.favorites.slice(0, 8);
     setCars(initialCars);
@@ -25,9 +35,12 @@ export default function Favorites() {
 
   return (
     <>
+      {/* Навігаційна панель */}
       <NavBar />
+      {/* Контейнер для відображення обраних автомобілів */}
       <Wrapper>
         <WrapperList>
+          {/* Відображення списку автомобілів або повідомлення про порожній список */}
           {cars.length ? (
             cars.map((favorite, index) => (
               <CarItem key={index} data={favorite} />
@@ -35,6 +48,7 @@ export default function Favorites() {
           ) : (
             <Text>Your favorites are currently empty...</Text>
           )}
+          {/* Відображення кнопки "Завантажити ще", якщо є ще автомобілі для завантаження */}
           {favoriteCars.favorites.length > cars.length && (
             <LoadMore variant="text" onClick={loadMore}>
               Load more
@@ -44,4 +58,6 @@ export default function Favorites() {
       </Wrapper>
     </>
   );
-}
+};
+
+export default Favorites;
